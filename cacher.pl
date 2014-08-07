3f76320d6921cea764744c089a8ca5cda2498cc4
Kalin KOZHUHAROV <me.kalin@gmail.com>
2014-08-08 00:12:04 +0900

    Fixing issue #2: TZ issue was half-fixed

--- cacher.pl
+++ cacher.pl
@@ -4,14 +4,17 @@ use strict;
 use utf8;
 use DateTime::Format::ISO8601;
 
-die ("\n[ERROR] Invalid number of arguments!\n\nUsage:\n\t$0 <sensor_id> <since_date>\n")
+die ("\n[ERROR] Invalid number of arguments!\n\nUsage:\n\t$0 <sensor_id> <fetch_since>\n")
 	unless (scalar @ARGV == 4);
 my $id = $ARGV[0];
-my $since_date = $ARGV[1];
+my $fetch_since = $ARGV[1];
 my $TIMEZONE = $ARGV[2];
 my $TZ = $ARGV[3];
 
-my $since_query = qx!TZ=${TIMEZONE} date +since=%d%%2F%m%%2F%Y+%H%%3A%M%%3A%S --date='${since_date}'!; chomp $since_query;
+# API server uses in UTC in query and results
+my $server_TZ='UTC';
+
+my $since_query = qx!TZ=${server_TZ} date +since=%d%%2F%m%%2F%Y+%H%%3A%M%%3A%S --date='${fetch_since}'!; chomp $since_query;
 qx!wget -q 'https://api.safecast.org/en-US/devices/$id/measurements.csv?${since_query}&order=captured_at+asc' -O cache/$id.tmp!;
 
 open(IN, "<cache/$id.tmp")
@@ -22,7 +25,7 @@ while(<IN>)
 {
 	my @R = split(/,/, $_, 5);
 	print OUT join(',', $R[0], $R[3]), "\n"
-		if ($R[0] =~ s#(\d{4}-\d\d-\d\d) (\d\d:\d\d:\d\d) UTC#DateTime::Format::ISO8601->parse_datetime(qq(${1}T${2}Z))->set_time_zone(${TIMEZONE}).${TZ}#e);
+		if ($R[0] =~ s#(\d{4}-\d\d-\d\d) (\d\d:\d\d:\d\d) ${server_TZ}#DateTime::Format::ISO8601->parse_datetime(qq(${1}T${2}Z))->set_time_zone(${TIMEZONE}).${TZ}#e);
 }
 close(OUT)
 	or die;
