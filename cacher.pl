51cf39e7fe36164ea2b0edd455fab4132dfd110d
Kalin KOZHUHAROV <me.kalin@gmail.com>
2018-01-23 13:51:36 +0100

    code/comments cleanup, no cahnge in output.

--- cacher.pl
+++ cacher.pl
@@ -16,11 +16,15 @@ my $AVG_WINDOW_LARGE = $ARGV[5];
 my $AVG_WINDOW_SMALL = $ARGV[6];
 
 die ("\n[ERROR] AVG_WINDOW_LARGE must be bigger than AVG_WINDOW_SMALL\n")
-	unless ($AVG_WINDOW_LARGE > $AVG_WINDOW_SMALL);
+	unless (${AVG_WINDOW_LARGE} > ${AVG_WINDOW_SMALL});
 
-# API server uses in UTC in query and results
-my $server_TZ='UTC';
 
+# {{{ defaults, updated 2018-01-22
+# -------------------------------------------------------------------
+my $DEFAULT_SERVER_TZ='UTC';		# API server uses in UTC in query and results
+my $DEFAULT_UPDATE_INTERVAL = 300;	# sensors usually send data every 300s
+my $DEFAULT_MISSED_INTERVALS = 5.0;	# consider malfunction, if 5 intervals are missing
+# }}}
 
 # {{{ Simple Moving Average filter implementation
 # -------------------------------------------------------------------
@@ -75,27 +79,22 @@ sub LP_filter_read
 }
 # }}}
 
-my $since_query = qx!TZ=${server_TZ} date +since=%d%%2F%m%%2F%Y+%H%%3A%M%%3A%S --date='${fetch_since}'!; chomp $since_query;
-my $until_query = qx!TZ=${server_TZ} date +until=%d%%2F%m%%2F%Y+%H%%3A%M%%3A%S --date='${fetch_until}'!; chomp $until_query;
+my $since_query = qx!TZ=${DEFAULT_SERVER_TZ} date +since=%d%%2F%m%%2F%Y+%H%%3A%M%%3A%S --date='${fetch_since}'!; chomp $since_query;
+my $until_query = qx!TZ=${DEFAULT_SERVER_TZ} date +until=%d%%2F%m%%2F%Y+%H%%3A%M%%3A%S --date='${fetch_until}'!; chomp $until_query;
 my $URL = "https://api.safecast.org/en-US/devices/$id/measurements.csv?${since_query}&${until_query}&unit=cpm&order=captured_at+asc";
-open(LOG, ">cache/$id.URL")
+
+# ::: save the URL
+open(LOG, ">cache/${id}.URL")
 	or die("$! , exitting");
 print LOG $URL;
 close(LOG)
 	or die("$! , exitting");
 
-my $cmd = "wget -q '${URL}' -O cache/$id.tmp";
+# ::: get the data from the URL in a temporary format
+my $cmd = "wget -q '${URL}' -O cache/${id}.tmp";
 qx!$cmd!;
 
-open(IN, "<cache/$id.tmp")
-	or die("$! , exitting");
-open(OUT, ">cache/$id.csv")
-	or die("$! , exitting");
-
-
-my $t_prev = -1;
-my $dt_prev = 36000;	# NOTE: Big number initially, 10h
-
+# ::: initialise statistics
 my @SMA_LARGE_bins = (); my %SMA_LARGE_config = ( 'window' => $AVG_WINDOW_LARGE);
 &SMA_filter_init(\@SMA_LARGE_bins, \%SMA_LARGE_config);
 
@@ -105,19 +104,28 @@ my @SMA_SMALL_bins = (); my %SMA_SMALL_config = ( 'window' => $AVG_WINDOW_SMALL)
 #my @LP_SMALL_bins = (); my %LP_SMALL_config = ( 'order' => 1, 'cutoff' => 60E-6, 'IV' => 0.0);
 #&LP_filter_init(\@LP_SMALL_bins, \%LP_SMALL_config);
 
+# ::: process each line ${id}.tmp -> ${id}.csv
+my $t_prev = -1;
+my $dt_prev = 36000;	# NOTE: Big number initially, 10h
+
+open(IN, "<cache/${id}.tmp")
+	or die("$! , exitting");
+open(OUT, ">cache/${id}.csv")
+	or die("$! , exitting");
 while(<IN>)
 {
 	my @R = split(/,/, $_, 5);
-	if ($R[0] =~ m#(\d{4}-\d\d-\d\d) (\d\d:\d\d:\d\d) ${server_TZ}#)
+	if ($R[0] =~ m#(\d{4}-\d\d-\d\d) (\d\d:\d\d:\d\d) ${DEFAULT_SERVER_TZ}#)	# reject things without timestamp
 	{
 		my $timestamp = DateTime::Format::ISO8601->parse_datetime(qq(${1}T${2}Z))->set_time_zone(${TIMEZONE});
 
-		# calculate difference
-		$t_prev = $timestamp->epoch() - 300	# defeault is 5min
+		# calculate time since previous update
+		$t_prev = $timestamp->epoch() - ${DEFAULT_UPDATE_INTERVAL}
 			if ($t_prev == -1);
 		my $dt = $timestamp->epoch() - $t_prev;
 		$t_prev = $timestamp->epoch();
 
+		# calculate statistics
 		&SMA_filter_update(\@SMA_LARGE_bins, \%SMA_LARGE_config, $R[3], $dt);
 		my $SMA_LARGE = &SMA_filter_read(\@SMA_LARGE_bins, \%SMA_LARGE_config);
 
@@ -127,10 +135,12 @@ while(<IN>)
 		#&LP_filter_update(\@LP_SMALL_bins, \%LP_SMALL_config, $R[3], $dt);
 		#my $LP_SMALL = &LP_filter_read(\@LP_SMALL_bins, \%LP_SMALL_config);
 
-		print OUT "\n"				# print blank line, if there was missing data (for gnuplot)
-			if ($dt > 5.0 * $dt_prev);
+		# print blank line (for gnuplot), if there is too much missing data
+		print OUT "\n"
+			if ($dt > ${DEFAULT_MISSED_INTERVALS} * $dt_prev);
 		$dt_prev = $dt;
 
+		# print the output in CSV
 		print OUT join(',', "${timestamp}${TZ}", $R[3], sprintf("%0.3f,%0.3f,%d\n", $SMA_LARGE, $SMA_SMALL, $dt));
 	}
 }
